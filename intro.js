0acc1d2c871ff7ef48f4d15eba483cb0ee0c90a1
Lukas Licon <lukas@licons.com>
2023-06-05 10:43:08 -0700

    working

--- intro.js
+++ intro.js
@@ -6,7 +6,7 @@ class intro extends Phaser.Scene {
     create() {
         this.cameras.main.fadeIn(1000, 0, 0, 0);
         this.add.text(560,560, "Core Gameplay Prototype").setFontSize(50);
-        this.add.text(760,760, "Click anywhere to start game 1.").setFontSize(20);
+        this.add.text(760,760, "Click anywhere to start next game.").setFontSize(20);
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0,0,0);
             this.time.delayedCall(1000, () => this.scene.start('npcScreen'));
